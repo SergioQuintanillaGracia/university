eaac2a47644e74b4fae83dc97798a54583d3d709
SergioQuintanillaGracia <sergioquintanillagracia@gmail.com>
2024-05-20 22:33:58 +0200

    QueueIntArray

---
+++ PRG/stacks_queues/QueueIntArray.java
@@ -0,0 +1,42 @@
+public class QueueIntArray {
+    private int arr[];
+    private int first, last, size;
+    private static final int MAX = 100000;
+
+    public QueueIntArray() {
+        arr = new int[MAX];
+        size = 0;
+        first = 0;
+        last = -1;
+    }
+
+    public void enqueue(int x) {
+        if (size == MAX) {
+            System.err.println("Size limit reached");
+            return;
+        }
+
+        last = (last + 1) % MAX;
+        arr[last] = x;
+        size++;
+    }
+
+    public int deque() {
+        int x = arr[first];
+        first = (first + 1) % MAX;
+        size--;
+        return x;
+    }
+
+    public int first() {
+        return arr[first];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+}
