921364d255d77e7de91c70dcebd335a3021bdcf0
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2024-06-02 17:43:34 +0200

    Added my own solution to checkErrors()

--- PRG/files/ex_p2_2023.java
+++ PRG/files/ex_p2_2023.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class ex_p2_2023 {
-    public static void checkErrors(String fileIn, String fileErr) throws FileNotFoundException {
+    public static void checkErrors_jonander(String fileIn, String fileErr) throws FileNotFoundException {
         Scanner input = new Scanner(new File(fileIn));
         PrintWriter errors = new PrintWriter(new File(fileErr));
 
@@ -33,5 +33,36 @@ public class ex_p2_2023 {
 
         input.close();
         errors.close();
+    }
+
+
+    public static void checkErrors(String fileIn, String fileErr) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(fileIn));
+        PrintWriter pw = new PrintWriter(new File(fileErr));
+        int line = 0;
+
+        while (sc.hasNext()) {
+            line++;
+            String[] data = sc.nextLine().trim().split("([ \t])+");
+
+            if (data.length != 3) {
+                pw.write("Error line %d: Incorrect number of data in a line.".formatted(line));
+            } else {
+                try {
+                    int d = Integer.parseInt(data[0]);
+                    int m = Integer.parseInt(data[1]);
+                    float db = Integer.parseInt(data[2]);
+
+                    if (db < 0) {
+                        pw.write("Error line %d: Negative value.".formatted(line));
+                    }
+                } catch (NumberFormatException e) {
+                    pw.write("Error line %d: Non-valid format.".formatted(line));
+                }
+            }
+        }
+
+        sc.close();
+        pw.close();
     }
 }
