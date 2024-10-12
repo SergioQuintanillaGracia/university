48516ca7af89344db99a4adf4b9a5c4d5a2e9f0d
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2024-10-12 23:12:21 +0200

    Finished battleship client, added README

--- RED/battleship/BattleshipClient.java
+++ RED/battleship/BattleshipClient.java
@@ -4,10 +4,18 @@ import java.net.NoRouteToHostException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Arrays;
+import java.util.List;
 
 public class BattleshipClient {
     public static void main(String[] args) {
+        int maxHits = 14;
+
         Socket socket = null;
+        PrintWriter pw = null;
+        Scanner serverSc = null;
+        Scanner clientSc = null;
 
         try {
             // Create a socket and connect to the server
@@ -15,36 +23,66 @@ public class BattleshipClient {
             System.out.println("Connected to the server");
 
             // Initialize the PrintWriter and Scanners
-            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-            Scanner sc = new Scanner(socket.getInputStream());
-            Scanner clientSc = new Scanner(System.in);
+            pw = new PrintWriter(socket.getOutputStream(), true);
+            serverSc = new Scanner(socket.getInputStream());
+            clientSc = new Scanner(System.in);
 
             // Ignore the first line, which contains protocol information
-            sc.nextLine();
+            serverSc.nextLine();
 
-            int hitCount = 0;
+            boolean continuePlaying = true;
+            int hitCount;
 
             // Client logic
-            while (hitCount < 14) {
-                // Get the board and display it
-                System.out.println("New iteration");
-                pw.println("BOARD");
-                printCount(sc, 10);
+            while (continuePlaying) {
+                System.out.print("\n\n");
 
-                System.out.printf("Guess > ");
+                // Reset the board in case this is not the first game
+                pw.println("RESET");
+                // Ignore the "OK" line returned by the server
+                serverSc.nextLine();
 
-                String guess = clientSc.nextLine();
-                pw.println("GUESS " + guess);
-                printCount(sc, 1);
+                // Initialize / reset hitCount to 0
+                hitCount = 0;
+
+                // Get and print the board for the first time
+                printBoard(pw, serverSc);
+
+                while (hitCount < maxHits) {
+                    System.out.printf("Game progress: %d/%d (%d%%)\n", hitCount, maxHits,
+                                      (int) ((double) hitCount / maxHits * 100));
+                    System.out.printf("Guess > ");
+
+                    if (handleGuess(pw, clientSc, serverSc)) {
+                        // Get a guess by the user
+                        // `handleGuess()` returns true if a boat was hit
+                        hitCount++;
+                    }
+
+                    // Get and print the new state of the board
+                    printBoard(pw, serverSc);
+                }
+
+                // The game has ended
+                // Ask the player if they want to continue playing
+                continuePlaying = getPlayAgainInput(clientSc);
             }
 
         } catch (UnknownHostException e) {
             System.out.println("Unknown host");
+        
         } catch (NoRouteToHostException e) {
             System.out.println("Couldn't reach the target host");
+        
         } catch (IOException e) {
             System.out.println("Couldn't connect to the server");
+        
         } finally {
+            // Close PrintWriter and Scanners
+            if (pw != null) pw.close();
+            if (serverSc != null) serverSc.close();
+            if (clientSc != null) clientSc.close();
+
             // Try to close the socket
             if (socket != null) {
                 try {
@@ -57,9 +95,106 @@ public class BattleshipClient {
         }
     }
 
-    public static void printCount(Scanner sc, int count) {
-        while (--count > 0) {
-            System.out.println(sc.nextLine());
+    public static void printCount(Scanner serverSc, int count) {
+        while (count-- > 0) {
+            System.out.println(serverSc.nextLine());
         }
+    }
+
+    public static void printBoard(PrintWriter pw, Scanner serverSc) {
+        pw.println("BOARD");
+
+        // Leave space on the left for the letters
+        System.out.printf("  ");
+
+        // Print top number row
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d ", i);
+        }
+
+        System.out.println();
+
+        // Print the board lines preceded by their corresponding letter
+        for (int i = 65; i <= 74; i++) {
+            System.out.printf("%c ", i);
+            printCount(serverSc, 1);
+        }
+    }
+
+    public static String getGuess(Scanner clientSc) {
+        String guess = clientSc.nextLine();
+
+        if (guess.length() == 2 || guess.length() == 3) {
+            // Check if the first character is a letter
+            if (Character.isLetter(guess.charAt(0))) {
+                // Check if the remaining characters are digits
+                try {
+                    Integer.parseInt(guess.substring(1, guess.length()));
+                    return guess.toUpperCase();
+                } catch (NumberFormatException e) { }
+            }
+        }
+
+        throw new InputMismatchException("Invalid guess");
+    }
+
+    /*
+     * Returns `true` if a boat is hit, and `false` otherwise.
+     */
+    public static boolean handleGuess(PrintWriter pw, Scanner clientSc, Scanner serverSc) {
+        boolean boatHit = false;
+
+        try {
+            // Get the user guess and send it to the server
+            String guess = getGuess(clientSc);
+            pw.println("GUESS " + guess);
+            
+            // React accordingly depending on the server's response
+            switch(serverSc.nextLine()) {
+                case "WATER":
+                    System.out.println("Water");
+                    break;
+                case "HIT":
+                    System.out.println("A boat was hit!");
+                    boatHit = true;
+                    break;
+                case "Coordinates out of bounds. Use coordinates like A1, B2...":
+                    System.out.println("Coordinates out of bounds");
+                    break;
+                case "Already guessed this position. Try another.":
+                    System.out.println("Already guessed this position, try another");
+                    break;
+                default:
+                    System.out.println("You managed to get the server to respond something that " +
+                                       "wasn't taken into account. You should be proud of yourself. " +
+                                       "Or not. You decide.");
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid guess, guesses must be in the form [letter][1-2 digits] (ex: C5, A10)");
+        }
+
+        return boatHit;
+    }
+
+    public static boolean getPlayAgainInput(Scanner clientSc) {
+        List<String> yesList = Arrays.asList("y", "ye", "yes", "ok", "yep", "s", "si", "sí");
+        List<String> noList = Arrays.asList("n", "no", "nope");
+        Boolean continuePlaying = null;
+
+        while (continuePlaying == null) {
+            System.out.printf("The game has finished! Do you want to play again? [Y/N] > ");
+            String input = clientSc.nextLine().toLowerCase();
+            
+            if (noList.contains(input)) {
+                continuePlaying = false;
+            } else if (yesList.contains(input)) {
+                continuePlaying = true;
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+
+        return continuePlaying;
     }
 }
--- RED/battleship/README.md
+++ RED/battleship/README.md
@@ -1,6 +1,17 @@
+## SUMMARY OF HOW THE CLIENT WORKS
+My Battleship game client works as follows:
+
+- The client connects to the server using a socket.
+- A `PrintWriter` and two `Scanners` are created, which are used to send requests to the server, and get the user's input and the server's responses.
+- A main loop starts, which resets the board and starts new games. It continues running until the user decides not to continue playing.
+- The game board is printed and the user is asked to make a guess. The input is validated and sent to the server, whose response is used to determine if a boat was hit or not.
+- The game board is updated. The user continues being asked to make a guess until 14 hits are made, which means that all boats have been sunk and the game ended.
+
 ## DESIGN CHOICES
-...
-
-
-## CHALLENGES
-- 
+- When creating the `Socket`, `PrintWriter`, and `Scanners`, `UnknownHostException`, `NoRouteToHostException` and `IOException` exceptions are handled. All of them are closed in the `finally` block.
+- I coded several functions to make the main `while` loop more readable, concise, and easier to understand:
+    - `printCount()`: Prints a specific number of lines read from the server socket.
+    - `printBoard()`: Requests and prints the game board. It prints a number row at the top, and a letter column at the left, so it's easier for the user to know the coordinates of the spot they want to guess.
+    - `getGuess()`: Asks the user for a coordinate, validates the input, and returns it. It throws `InputMismatchException` if the input is invalid, which is handled in `handleGuess()`.
+    - `handleGuess()`: Handles the different possible server outputs to a guess.
+    - `getPlayAgainInput()`: Asks the user whether they want to play another game. It checks if their answer is in arrays containing words for "yes" or "no" to determine their decision.
