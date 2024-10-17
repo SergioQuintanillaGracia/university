0bf1c4ffb8aa657dabc5b9b457564e424cb68af7
Sergio Quintanilla <sergioquintanillagracia@gmail.com>
2024-10-17 13:56:07 +0200

    Finished complex.c

--- FSO/Tests/complex.c
+++ FSO/Tests/complex.c
@@ -5,6 +5,18 @@ typedef struct {
     float imag;
 } Complex;
 
+void printComplex(Complex *pc) {
+    printf("%f + %fi\n", pc->real, pc->imag);
+}
+
 int main() {
-    // TODO
+    Complex c = (Complex){5, 2.22};
+    Complex *pc = &c;
+
+    pc->imag = 1.33;
+
+    printf("Enter new i value: ");
+    scanf("%f", &pc->imag);
+
+    printComplex(pc);
 }
