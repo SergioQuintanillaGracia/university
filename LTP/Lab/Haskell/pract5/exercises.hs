23f5ae6e73c0efc786ef227209298dba17559ff6
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2024-11-20 09:29:56 +0100

    LTP exercises up to 20

--- LTP/Lab/Haskell/pract5/exercises.hs
+++ LTP/Lab/Haskell/pract5/exercises.hs
@@ -87,3 +87,76 @@ borrow db b p = (p, b) : db
 
 return' :: Database -> (Person, Book) -> Database
 return' db (p,b) = [(pers,book) | (pers,book) <- db, (p,b) /= (pers,book)]
+
+
+-- EX 12
+data Tree a = Leaf a | Branch (Tree a) (Tree a) deriving Show
+
+symmetric :: Tree a -> Tree a
+symmetric (Leaf a) = Leaf a
+symmetric (Branch a b) = Branch (symmetric b) (symmetric a)
+
+
+-- EX 13
+listToTree :: [a] -> Tree a
+listToTree [x] = Leaf x
+listToTree xs = Branch (listToTree(take size xs)) (listToTree(drop size xs))
+    where size = length xs `div` 2
+
+treeToList :: Tree a -> [a]
+treeToList (Leaf x) = [x]
+treeToList (Branch a b) = treeToList a ++ treeToList b
+
+
+-- EX 14
+data BinTreeInt = Void | Node Int BinTreeInt BinTreeInt deriving Show
+
+insTree :: Int -> BinTreeInt -> BinTreeInt
+insTree x Void = Node x Void Void
+insTree x (Node y a b)
+    | x < y = Node y (insTree x a) b
+    | otherwise = Node y a (insTree x b)
+
+
+-- EX 15
+creaTree :: [Int] -> BinTreeInt
+creaTree [] = Void
+creaTree (x:xs) = insTree x (creaTree xs)
+
+
+-- EX 16
+treeElem :: Int -> BinTreeInt -> Bool
+treeElem x Void = False
+treeElem x (Node a left right)
+    | x < a = treeElem x left
+    | x > a = treeElem x right
+    | x == a = True
+
+
+-- EX 17
+repeated :: Int -> [Int] -> Int
+repeated n [] = 0
+repeated n (x:xs)
+    | n == x = 1 + repeated n xs
+    | otherwise = repeated n xs
+
+
+-- EX 18
+concat' :: [[a]] -> [a]
+concat' [] = []
+concat' (x:xs) = x ++ concat' xs
+
+
+-- EX 19
+square :: Int -> Int
+square x = x * x
+res = (sum . map square . filter even) [1..10]
+-- Returns the addition of the squares of the even numbers from 1 to 10
+
+
+-- EX 20
+-- data Tree a = Leaf a | Branch (Tree a) (Tree a) deriving Show
+
+numLeaves :: Tree a -> Int
+numLeaves (Leaf a) = 1
+numLeaves (Branch left right) = numLeaves left + numLeaves right
