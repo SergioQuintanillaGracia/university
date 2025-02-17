a5d3d46bd27a8debd932d6dc1f91779364caf82b
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2025-02-17 12:33:58 +0100

    Pool 1

--- CSD/lab/pract1/Pool0.java
+++ CSD/lab/pract1/Pool0.java
@@ -1,9 +1,22 @@
 // CSD feb 2015 Juansa Sendra
 
-public class Pool0 extends Pool {	//free access
-    public void init(int ki, int cap) 			{}
-    public void kidSwims() 		{log.swimming();}
-    public void kidRests() 		{log.resting(); }
-    public void instructorSwims()	{log.swimming();}
-    public void instructorRests() 	{log.resting(); }
+public class Pool0 extends Pool { //free access
+
+    public void init(int ki, int cap) {}
+
+    public void kidSwims() {
+        log.swimming();
+    }
+
+    public void kidRests() {
+        log.resting();
+    }
+
+    public void instructorSwims() {
+        log.swimming();
+    }
+
+    public void instructorRests() {
+        log.resting();
+    }
 }
--- CSD/lab/pract1/Pool1.java
+++ CSD/lab/pract1/Pool1.java
@@ -1,9 +1,47 @@
 // CSD feb 2015 Juansa Sendra
 
-public class Pool1 extends Pool {   //no kids alone
-    public void init(int ki, int cap)           {}
-    public void kidSwims()      {log.swimming();}
-    public void kidRests()      {log.resting(); }
-    public void instructorSwims()   {log.swimming();}
-    public void instructorRests()   {log.resting(); }
+public class Pool1 extends Pool { //no kids alone
+
+    private int kidsSwimming = 0;
+    private int instructorsSwimming = 0;
+
+    public void init(int ki, int cap) {}
+
+    public synchronized void kidSwims() throws InterruptedException {
+        while (instructorsSwimming == 0) {
+            log.waitingToSwim();
+            wait();
+        }
+
+        kidsSwimming++;
+
+        log.swimming();
+    }
+
+    public synchronized void kidRests() {
+        kidsSwimming--;
+
+        notifyAll();
+
+        log.resting();
+    }
+
+    public synchronized void instructorSwims() {
+        instructorsSwimming++;
+
+        notifyAll();
+
+        log.swimming();
+    }
+
+    public synchronized void instructorRests() throws InterruptedException {
+        while (kidsSwimming > 0) {
+            log.waitingToRest();
+            wait();
+        }
+
+        instructorsSwimming--;
+
+        log.resting();
+    }
 }
