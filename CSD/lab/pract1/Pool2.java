f206cbf65b7f98cf4fb822666dcb289f2694c2e0
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2025-02-17 12:44:54 +0100

    Pool 2

--- CSD/lab/pract1/Pool2.java
+++ CSD/lab/pract1/Pool2.java
@@ -1,9 +1,50 @@
 // CSD feb 2015 Juansa Sendra
 
-public class Pool2 extends Pool{ //max kids/instructor
-    public void init(int ki, int cap)           {}
-    public void kidSwims()      {log.swimming();}
-    public void kidRests()      {log.resting(); }
-    public void instructorSwims()   {log.swimming();}
-    public void instructorRests()   {log.resting(); }
+public class Pool2 extends Pool { //max kids/instructor
+
+    private int kidsPerInstructor;
+    private int kidsSwimming = 0;
+    private int instructorsSwimming = 0;
+
+    public void init(int ki, int cap) {
+        kidsPerInstructor = ki;
+    }
+
+    public synchronized void kidSwims() throws InterruptedException {
+        while (kidsSwimming >= instructorsSwimming * kidsPerInstructor) {
+            log.waitingToSwim();
+            wait();
+        }
+
+        kidsSwimming++;
+
+        log.swimming();
+    }
+
+    public synchronized void kidRests() {
+        kidsSwimming--;
+
+        notifyAll();
+
+        log.resting();
+    }
+
+    public synchronized void instructorSwims() {
+        instructorsSwimming++;
+
+        notifyAll();
+
+        log.swimming();
+    }
+
+    public synchronized void instructorRests() throws InterruptedException {
+        while (kidsSwimming > (instructorsSwimming - 1) * kidsPerInstructor) {
+            log.waitingToRest();
+            wait();
+        }
+
+        instructorsSwimming--;
+
+        log.resting();
+    }
 }
