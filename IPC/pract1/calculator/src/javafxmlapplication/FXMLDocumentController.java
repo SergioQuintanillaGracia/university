fa1bb982c7fa2aadf876e95d253de70c3ee08b8f
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2025-02-20 21:53:15 +0100

    Finished login form

--- IPC/pract1/calculator/src/javafxmlapplication/FXMLDocumentController.java
+++ IPC/pract1/calculator/src/javafxmlapplication/FXMLDocumentController.java
@@ -12,23 +12,23 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 
 /**
  *
  * @author jsoler
  */
 public class FXMLDocumentController implements Initializable {
+    //========================================================
+    // objects defined into FXML file with fx:id 
+    @FXML
+    private Button buttonClick;
+    @FXML
     private Label labelMessage;
-    @FXML
-    private TextField userName;
-    @FXML
-    private Text userMessage;
     
     //=========================================================
     // event handler, fired when button is clicked or 
     //                      when the button has the focus and enter is pressed
+    @FXML
     private void handleButtonAction(ActionEvent event) {
         labelMessage.setText("Hello, this is your first JavaFX project - IPC");
     }
@@ -39,10 +39,5 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-
-    @FXML
-    private void loginClicked(ActionEvent event) {
-        userMessage.setText("Welcome: " + userName.getText());
-    }
     
 }
--- IPC/pract1/calculator/src/javafxmlapplication/JavaFXMLApplication.java
+++ IPC/pract1/calculator/src/javafxmlapplication/JavaFXMLApplication.java
@@ -18,7 +18,7 @@ public class JavaFXMLApplication extends Application {
     public void start(Stage stage) throws Exception {
         //======================================================================
         // 1- creación del grafo de escena a partir del fichero FXML
-        FXMLLoader loader = new  FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
+        FXMLLoader loader= new  FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
         Parent root = loader.load();
         //======================================================================
         // 2- creación de la escena con el nodo raiz del grafo de escena
@@ -28,8 +28,7 @@ public class JavaFXMLApplication extends Application {
         //     - configuracion del stage
         //     - se muestra el stage de manera no modal mediante el metodo show()
         stage.setScene(scene);
-        stage.setTitle("Login");
-        stage.setResizable(false);
+        stage.setTitle("start PROJECT - IPC:");
         stage.show();
     }
 
