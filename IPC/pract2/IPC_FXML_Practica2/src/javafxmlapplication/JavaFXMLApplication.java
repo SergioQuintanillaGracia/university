fc1bc5ffa71c20d553ae66600bd75e41b59f1b46
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2025-03-06 18:35:26 +0100

    Update JavaFXMLApplication.java

--- IPC/pract2/IPC_FXML_Practica2/src/javafxmlapplication/JavaFXMLApplication.java
+++ IPC/pract2/IPC_FXML_Practica2/src/javafxmlapplication/JavaFXMLApplication.java
@@ -28,7 +28,8 @@ public class JavaFXMLApplication extends Application {
         //     - configuracion del stage
         //     - se muestra el stage de manera no modal mediante el metodo show()
         stage.setScene(scene);
-        stage.setTitle("start PROJECT - IPC:");
+        stage.setTitle("Circle thing");
+        stage.setResizable(false);
         stage.show();
     }
 
