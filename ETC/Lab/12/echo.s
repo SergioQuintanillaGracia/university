759c74f17d92c56f89cb5622e14f939644efef46
Sergio Quintanilla <96151049+SergioQuintanillaGracia@users.noreply.github.com>
2025-03-26 10:26:24 +0100

    Pract 12

--- ETC/Lab/12/echo.s
+++ ETC/Lab/12/echo.s
@@ -19,7 +19,6 @@
     	.globl __start	
 
 __start:			
-
 	li $a0, 'P'		# 
 	jal putchar		# putchar('P')
 	li $a0, '1'		# 
@@ -37,24 +36,41 @@ loop:
 	move $a0, $v0		 #
 	beq $a0, $t0, finish # Detect ESC key
 	jal putchar		     # putchar($a0)
+	# jal putcode
+	li $a0, 13
+	jal putchar
 	b loop
 finish:	
 	li $v0, 10
 	syscall			     # exit
 	
 	
-	
 getchar:        # $v0 = getchar()
-    ### TO BE COMPLETED
-
-
+    li $t2, 0xffff0000
+	
+	wait:   # Wait for bit R == 1
+		lw $t1, 0($t2)
+		andi $t1, $t1, 1
+		beqz $t1, wait
+		
+	lw $v0, 4($t2)
 	jr $ra      # return from getchar
-
-
 
 
 putchar:        # putchar($a0)
     ### TO BE COMPLETED
-
-
+	li $t2, 0xffff0008
+	
+	wait2:
+		lw $t1, 0($t2)
+		andi $t1, $t1, 1
+		beqz $t1, wait2
+	
+	sw $a0, 4($t2)	
 	jr $ra      # return from putchar
+
+
+putcode:
+	li $v0, 1
+	syscall
+	jr $ra
