85a27bc7298ca219ff3b94c6ea5859dbbac751f9
Sergio Quintanilla <sergioquintanillagracia@gmail.com>
2025-04-10 18:01:17 +0200

    IPC P3 progress

---
+++ IPC/pract3/IPC_FXML_ListView-main/src/tarea2/controlador/VistaPersonaController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import modelo.Persona;
+
+/**
+ * FXML Controller class
+ *
+ * @author jsoler
+ */
+public class VistaPersonaController implements Initializable {
+
+    @FXML
+    private TextField nombreTextField;
+    @FXML
+    private TextField apellidosTextField;
+    
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void aceptar(ActionEvent event) {
+
+       
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+        nombreTextField.getScene().getWindow().hide();
+    }
+
+
+    
+}
